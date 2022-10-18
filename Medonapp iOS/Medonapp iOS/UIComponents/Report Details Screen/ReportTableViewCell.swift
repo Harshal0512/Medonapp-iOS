2fbc122f95fc75242b9197626bde6faa4fb46844
Harshal Kulkarni <harshal.kulkarni0512@gmail.com>
2022-10-18 22:52:26 +0530

    TableView in Report Details
    Added TableView in Report Details VC

---
+++ Medonapp iOS/Medonapp iOS/UIComponents/Report Details Screen/ReportTableViewCell.swift
@@ -0,0 +1,61 @@
+//
+//  ReportTableViewCell.swift
+//  Medonapp iOS
+//
+//  Created by Harshal Kulkarni on 18/10/22.
+//
+
+import UIKit
+
+class ReportTableViewCell: UITableViewCell {
+    
+    static let identifier = "ReportTableViewCell"
+    
+    static func nib() -> UINib {
+        return UINib(nibName: "ReportTableViewCell", bundle: nil)
+    }
+    
+    @IBOutlet private var cellContentView: UIView!
+    @IBOutlet private var iconView: UIView!
+    @IBOutlet private var iconImage: UIImageView!
+    @IBOutlet private var reportTitle: UILabel!
+    @IBOutlet private var moreIcon: UIImageView!
+    
+    public func configure(icon: UIImage, reportTitle: String, reportCellVariant: reportVariant) {
+        cellContentView.layer.cornerRadius = 28
+        self.iconImage.image = icon
+        self.moreIcon.transform = moreIcon.transform.rotated(by: CGFloat(Double.pi / 2))
+        
+        self.iconView.backgroundColor = .clear
+        self.iconView.layer.cornerRadius = 20
+        
+        self.reportTitle.text = reportTitle
+        
+        self.reportTitle.textColor = .black
+        
+        switch reportCellVariant {
+        case .user:
+            cellContentView.backgroundColor = UIColor(red: 0.86, green: 0.93, blue: 0.98, alpha: 1.00)
+            iconView.layer.borderColor = UIColor(red: 0.11, green: 0.42, blue: 0.64, alpha: 1.00).cgColor
+            iconView.layer.borderWidth = 2
+            iconImage.image = iconImage.image!.withTintColor(UIColor(red: 0.11, green: 0.42, blue: 0.64, alpha: 1.00))
+        case .family:
+            cellContentView.backgroundColor = UIColor(red: 0.84, green: 1.00, blue: 0.95, alpha: 1.00)
+            iconView.layer.borderColor = UIColor(red: 0.00, green: 0.54, blue: 0.37, alpha: 1.00).cgColor
+            iconView.layer.borderWidth = 2
+            iconImage.image = iconImage.image!.withTintColor(UIColor(red: 0.00, green: 0.54, blue: 0.37, alpha: 1.00))
+        }
+    }
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
--- Medonapp iOS/Medonapp iOS/ViewControllers/Report Tab/ReportDetailsViewController.swift
+++ Medonapp iOS/Medonapp iOS/ViewControllers/Report Tab/ReportDetailsViewController.swift
@@ -16,23 +16,29 @@ class ReportDetailsViewController: UIViewController {
     
     private var backButton: UIImageView?
     private var navTitle: UILabel?
+    private var reportsHistoryTable: UITableView?
     
     private var reportsTopView: ReportCellWithIconAndDescription?
     
     var reportsVariant: reportVariant = .user
-
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         navigationController?.navigationBar.prefersLargeTitles = false
         navigationController?.navigationItem.largeTitleDisplayMode = .never
         navigationController?.isNavigationBarHidden = true
-
+        
         initialise()
         setupUI()
         setConstraints()
+        
+        reportsHistoryTable?.register(ReportTableViewCell.nib(), forCellReuseIdentifier: ReportTableViewCell.identifier)
+        reportsHistoryTable?.delegate = self
+        reportsHistoryTable?.dataSource = self
     }
-
+    
     func initialise() {
     }
     
@@ -57,12 +63,17 @@ class ReportDetailsViewController: UIViewController {
         } else if reportsVariant == .family {
             setupFamilyReportsUIWithConstraints()
         }
+        
+        reportsHistoryTable = UITableView()
+        reportsHistoryTable?.separatorStyle = .none
+        view.addSubview(reportsHistoryTable!)
     }
     
     func setConstraints() {
         backButton?.translatesAutoresizingMaskIntoConstraints = false
         navTitle?.translatesAutoresizingMaskIntoConstraints = false
         reportsTopView?.translatesAutoresizingMaskIntoConstraints = false
+        reportsHistoryTable?.translatesAutoresizingMaskIntoConstraints = false
         
         
         backButton?.topAnchor.constraint(equalTo: view.topAnchor, constant: 65).isActive = true
@@ -78,19 +89,70 @@ class ReportDetailsViewController: UIViewController {
         reportsTopView!.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 27).isActive = true
         reportsTopView!.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -27).isActive = true
         reportsTopView!.heightAnchor.constraint(equalToConstant: 93).isActive = true
+        
+        reportsHistoryTable?.topAnchor.constraint(equalTo: reportsTopView!.bottomAnchor, constant: 10).isActive = true
+        reportsHistoryTable?.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 10).isActive = true
+        reportsHistoryTable?.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -10).isActive = true
+        reportsHistoryTable?.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -10).isActive = true
     }
     
     func setupUserReportsUIWithConstraints() {
         reportsTopView = ReportCellWithIconAndDescription.instantiate(viewBackgroundColor: .white, icon: UIImage(named: "documentIcon")!.withTintColor(UIColor(red: 0.11, green: 0.42, blue: 0.64, alpha: 1.00)), iconBackgroundColor: UIColor(red: 0.86, green: 0.93, blue: 0.98, alpha: 1.00), title: "My Reports", numberOfFiles: 8, showOutline: false, showMoreIcon: false)
         view.addSubview(reportsTopView!)
+        
+        //future additions
     }
     
     func setupFamilyReportsUIWithConstraints() {
         reportsTopView = ReportCellWithIconAndDescription.instantiate(viewBackgroundColor: .white, icon: UIImage(named: "documentIcon")!.withTintColor(UIColor(red: 0.00, green: 0.54, blue: 0.37, alpha: 1.00)), iconBackgroundColor: UIColor(red: 0.84, green: 1.00, blue: 0.95, alpha: 1.00), title: "Family Reports", numberOfFiles: 8, showOutline: false, showMoreIcon: false)
         view.addSubview(reportsTopView!)
+        
+        //future additions
     }
     
     @objc func handleBackAction(_ sender: UITapGestureRecognizer? = nil) {
         self.dismiss(animated: true)
+    }
+}
+
+extension ReportDetailsViewController: UITableViewDelegate, UITableViewDataSource {
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 3
+    }
+    
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        if section == 0 {
+            return "6 Oct. 2022 -------------"
+        } else if section == 1 {
+            return "11 Oct. 2022 -------------"
+        } else if section == 2 {
+            return "12 Nov. 2022 -------------"
+        } else {
+            return ""
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 3
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 133
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = reportsHistoryTable!.dequeueReusableCell(withIdentifier: ReportTableViewCell.identifier, for: indexPath) as! ReportTableViewCell
+        if reportsVariant == .user {
+            cell.configure(icon: UIImage(named: "documentIcon")!, reportTitle: "X-Ray Report", reportCellVariant: .user)
+        } else if reportsVariant == .family {
+            cell.configure(icon: UIImage(named: "documentIcon")!, reportTitle: "X-Ray Report", reportCellVariant: .family)
+        }
+        
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        reportsHistoryTable?.scrollToRow(at: indexPath, at: .middle, animated: true)
+        reportsHistoryTable?.deselectRow(at: indexPath, animated: true)
     }
 }
