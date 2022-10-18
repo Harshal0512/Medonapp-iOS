bef0db766a5bd12c1297f9ed09bf7049841555ec
Harshal Kulkarni <harshal.kulkarni0512@gmail.com>
2022-10-18 10:48:11 +0530

    Models, Utils
    Added Generic JSON Util methods, Added temporary doctor model

---
+++ Medonapp iOS/Medonapp iOS/Models/Doctor.swift
@@ -0,0 +1,155 @@
+//
+//  Doctor.swift
+//  Medonapp iOS
+//
+//  Created by Harshal Kulkarni on 17/10/22.
+//
+
+// This file was generated from JSON Schema using quicktype, do not modify it directly.
+// To parse the JSON, add this file to your project and do:
+//
+//   let doctor = try? newJSONDecoder().decode(Doctor.self, from: jsonData)
+
+//
+// To parse values from Alamofire responses:
+//
+//   Alamofire.request(url).responseDoctor { response in
+//     if let doctor = response.result.value {
+//       ...
+//     }
+//   }
+
+import Foundation
+import Alamofire
+
+// MARK: - Doctor
+class Doctor: Codable {
+    var id: Int?
+    var credential: Credential?
+    var profileImagePath: String?
+    var name: Name?
+    var address: Address?
+    var mobile, gender, specialization: String?
+    var experience: Int?
+    var about: [String]?
+    var fees: Int?
+    var liveStatus: Bool?
+    var avgRating: Int?
+    var availableFromInWeekdays, availableFromInWeekends: String?
+    var maxAppointmentsInWeekdays, maxAppointmentsInWeekends: Int?
+    var appointmentDuration: String?
+    var weekdayAppointmentSlots, weekendAppointmentSlots: [String]?
+    var patientCount: Int?
+
+    init(id: Int?, credential: Credential?, profileImagePath: String?, name: Name?, address: Address?, mobile: String?, gender: String?, specialization: String?, experience: Int?, about: [String]?, fees: Int?, liveStatus: Bool?, avgRating: Int?, availableFromInWeekdays: String?, availableFromInWeekends: String?, maxAppointmentsInWeekdays: Int?, maxAppointmentsInWeekends: Int?, appointmentDuration: String?, weekdayAppointmentSlots: [String]?, weekendAppointmentSlots: [String]?, patientCount: Int?) {
+        self.id = id
+        self.credential = credential
+        self.profileImagePath = profileImagePath
+        self.name = name
+        self.address = address
+        self.mobile = mobile
+        self.gender = gender
+        self.specialization = specialization
+        self.experience = experience
+        self.about = about
+        self.fees = fees
+        self.liveStatus = liveStatus
+        self.avgRating = avgRating
+        self.availableFromInWeekdays = availableFromInWeekdays
+        self.availableFromInWeekends = availableFromInWeekends
+        self.maxAppointmentsInWeekdays = maxAppointmentsInWeekdays
+        self.maxAppointmentsInWeekends = maxAppointmentsInWeekends
+        self.appointmentDuration = appointmentDuration
+        self.weekdayAppointmentSlots = weekdayAppointmentSlots
+        self.weekendAppointmentSlots = weekendAppointmentSlots
+        self.patientCount = patientCount
+    }
+    
+    static private var doctors: [Doctor] = []
+    
+    static func getDoctors() -> [Doctor] {
+        return doctors
+    }
+    
+    static func clearDoctors() {
+        doctors = []
+    }
+    
+    static func refreshDoctors(completionHandler: @escaping () -> ()) {
+        //Refresh Doctors from API
+    }
+    
+    static func initDoctors(doctors: [Doctor]) {
+        self.doctors = doctors
+    }
+}
+
+//
+// To parse values from Alamofire responses:
+//
+//   Alamofire.request(url).responseAddress { response in
+//     if let address = response.result.value {
+//       ...
+//     }
+//   }
+
+// MARK: - Address
+class Address: Codable {
+    var address, state, city, postalCode: String?
+    var latitude, longitude: JSONNull?
+    var fullAddressWithoutState, fullAddressWithoutPostalCode, fullAddress: String?
+
+    init(address: String?, state: String?, city: String?, postalCode: String?, latitude: JSONNull?, longitude: JSONNull?, fullAddressWithoutState: String?, fullAddressWithoutPostalCode: String?, fullAddress: String?) {
+        self.address = address
+        self.state = state
+        self.city = city
+        self.postalCode = postalCode
+        self.latitude = latitude
+        self.longitude = longitude
+        self.fullAddressWithoutState = fullAddressWithoutState
+        self.fullAddressWithoutPostalCode = fullAddressWithoutPostalCode
+        self.fullAddress = fullAddress
+    }
+}
+
+//
+// To parse values from Alamofire responses:
+//
+//   Alamofire.request(url).responseCredential { response in
+//     if let credential = response.result.value {
+//       ...
+//     }
+//   }
+
+// MARK: - Credential
+class Credential: Codable {
+    var email, password, createdOn, lastLogin: String?
+
+    init(email: String?, password: String?, createdOn: String?, lastLogin: String?) {
+        self.email = email
+        self.password = password
+        self.createdOn = createdOn
+        self.lastLogin = lastLogin
+    }
+}
+
+//
+// To parse values from Alamofire responses:
+//
+//   Alamofire.request(url).responseName { response in
+//     if let name = response.result.value {
+//       ...
+//     }
+//   }
+
+// MARK: - Name
+class Name: Codable {
+    var firstName, middleName, lastName, fullName: String?
+
+    init(firstName: String?, middleName: String?, lastName: String?, fullName: String?) {
+        self.firstName = firstName
+        self.middleName = middleName
+        self.lastName = lastName
+        self.fullName = fullName
+    }
+}
--- Medonapp iOS/Medonapp iOS/Utils/Utils.swift
+++ Medonapp iOS/Medonapp iOS/Utils/Utils.swift
@@ -48,18 +48,69 @@ class Utils {
     
     class func openWhatsApp(numberWithCountryCode: String) {
         let urlWhats = "whatsapp://send?phone=\(numberWithCountryCode)&abid=12354&text=Hello Doctor,"
-            if let urlString = urlWhats.addingPercentEncoding(withAllowedCharacters: NSCharacterSet.urlQueryAllowed) {
-                if let whatsappURL = URL(string: urlString) {
-                    if UIApplication.shared.canOpenURL(whatsappURL) {
-                        if #available(iOS 10, *) {
-                            UIApplication.shared.open(whatsappURL, options: [:], completionHandler:nil)
-                        } else {
-                            UIApplication.shared.openURL(whatsappURL)
-                        }
+        if let urlString = urlWhats.addingPercentEncoding(withAllowedCharacters: NSCharacterSet.urlQueryAllowed) {
+            if let whatsappURL = URL(string: urlString) {
+                if UIApplication.shared.canOpenURL(whatsappURL) {
+                    if #available(iOS 10, *) {
+                        UIApplication.shared.open(whatsappURL, options: [:], completionHandler:nil)
                     } else {
-                        
+                        UIApplication.shared.openURL(whatsappURL)
                     }
+                } else {
+                    
                 }
             }
+        }
     }
 }
+
+
+// MARK: - Helper functions for creating encoders and decoders
+
+func newJSONDecoder() -> JSONDecoder {
+    let decoder = JSONDecoder()
+    if #available(iOS 10.0, OSX 10.12, tvOS 10.0, watchOS 3.0, *) {
+        decoder.dateDecodingStrategy = .iso8601
+    }
+    return decoder
+}
+
+func newJSONEncoder() -> JSONEncoder {
+    let encoder = JSONEncoder()
+    if #available(iOS 10.0, OSX 10.12, tvOS 10.0, watchOS 3.0, *) {
+        encoder.dateEncodingStrategy = .iso8601
+    }
+    return encoder
+}
+
+// MARK: - Encode/decode helpers
+
+class JSONNull: Codable, Hashable {
+
+    public static func == (lhs: JSONNull, rhs: JSONNull) -> Bool {
+        return true
+    }
+
+    public var hashValue: Int {
+        return 0
+    }
+
+    public func hash(into hasher: inout Hasher) {
+        // No-op
+    }
+
+    public init() {}
+
+    public required init(from decoder: Decoder) throws {
+        let container = try decoder.singleValueContainer()
+        if !container.decodeNil() {
+            throw DecodingError.typeMismatch(JSONNull.self, DecodingError.Context(codingPath: decoder.codingPath, debugDescription: "Wrong type for JSONNull"))
+        }
+    }
+
+    public func encode(to encoder: Encoder) throws {
+        var container = encoder.singleValueContainer()
+        try container.encodeNil()
+    }
+}
+
